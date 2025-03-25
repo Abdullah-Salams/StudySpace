436c0c0d6011c689ddd5b861e157f292d571a425
joshuah3617 <JoshTheGamer4896@gmail.com>
2025-03-25 17:39:00 -0400

    Backend has a few changes just for debugging. Will be cleared later. App.js is the frontend file that has the basic frontend connection with backend and shows the basic array with the 3 datas being shown.

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ def hello_world():
     try:
         with MongoClient(uri, server_api=ServerApi('1')) as client:
             client.admin.command('ping')
+            print("MongoDB connection successful!")
             return "Connected to MongoDB!"
     except Exception as e:
+        print("Error connecting to MongoDB:", e)
         return str(e)
 
 @app.route('/bookings', methods=['GET'])
@@ -24,10 +26,16 @@ def get_bookings():
         with MongoClient(uri, server_api=ServerApi('1')) as client:
             db = client["study_room_booking"]
             collection = db["bookings"]
+
+            print("Connected to MongoDB, fetching books...")
+
             bookings = list(collection.find({}, {"_id": 0}))
+            print("Bookings from DB:", bookings)
+
             return jsonify({"bookings": bookings})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        print("Error fetching bookings:", e)
+        return jsonify({"error": str(e)})
 @app.route('/bookings', methods=['POST'])
 def add_booking():
     try:
@@ -38,8 +46,4 @@ def add_booking():
             collection.insert_one(data)
             return jsonify({"message": "Booking added!"})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+        return jsonify({"error": str(e)})
